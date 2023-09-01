40d77b11fb1b447dfd73b67e64d621c36eba0b7c
Syed Abidur Rahman <abidur.rahman@shohoz.com>
2023-09-01 23:33:28 +0600

    Add ServiceProvider for the package.

---
+++ src/HttpRequestApiLogServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sarahman\HttpRequestApiLog;
+
+use Illuminate\Support\ServiceProvider;
+
+class HttpRequestApiLogServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        $this->package('sarahman/laravel-http-request-api-log', null, __DIR__);
+    }
+
+    public function register()
+    {
+
+    }
+}
