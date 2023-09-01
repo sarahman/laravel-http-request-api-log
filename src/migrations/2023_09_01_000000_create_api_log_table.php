6d9d93c9e207bfd4dbd37807e9fc586b363d8bb0
Syed Abidur Rahman <abidur.rahman@shohoz.com>
2023-09-01 15:41:05 +0600

    Add migration file to created the ApiLog db table.

---
+++ src/migrations/2023_09_01_000000_create_api_log_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApiLogTable extends Migration
+{
+    public function up()
+    {
+        $config = Config::get('laravel-http-request-api-log::config');
+
+        Schema::connection($config['database_connection'])->create($config['table_name'], function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('client', 50)->index();
+            $table->string('method', 6);
+            $table->string('endpoint', 2083)->index();
+            $table->longText('params');
+            $table->char('response_code', 3);
+            $table->longText('response');
+            $table->timestamps();
+            $table->index('created_at');
+        });
+    }
+
+    public function down()
+    {
+        $config = Config::get('laravel-http-request-api-log::config');
+
+        Schema::connection($config['database_connection'])->dropIfExists($config['table_name']);
+    }
+}
